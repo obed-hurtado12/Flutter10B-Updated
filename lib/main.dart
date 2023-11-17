87a1f1584b6e1d57cd700db2385f12636a9ae3ce
obed-hurtado12 <20193tn117@utez.edu.mx>
2023-11-17 15:20:09 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainApp extends StatelessWidget {
       routes: {
         '/': (context) => const Splash(),
         '/menu': (context) => const Menu(),
-      },
+      }, 
     );
   }
 }
